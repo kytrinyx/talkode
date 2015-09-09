089ed2d757117a7a195e03f6dda2f413b09ad187
Damon Davison <damon@curatur.com>
2015-09-09 11:13:17 -0400

    Add multiple language support

--- colorize.sh
+++ colorize.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-for file in `ls -1 . | grep .rb`
-do
-  outfile="colorized/`basename $file .rb`.rtf"
-  style="style=overkill"
-  font="fontface=Source Code Pro"
-  lexer="ruby"
-  format="rtf"
-  pygmentize -l $lexer -P $style -P "$font" -f $format -o $outfile $file
-done
+target_dir="colorized"
+font_name="Source Code Pro"
+style_name="presentation"
+format="rtf"
+
+style="style=${style_name}"
+font="fontface=${font_name}"
+
+mkdir -p $target_dir
+
+function colorize() {
+  lexer=$1
+  extension=$2
+  for file in `ls -1 . | grep ${extension}`
+  do
+    filename=$(basename $file $extension).${extension}.rtf
+    outfile="${target_dir}/${filename}"
+    pygmentize -l $lexer -P $style -P "$font" -f $format -o $outfile $file
+  done
+}
+
+colorize "ruby" ".rb"
+colorize "js" ".js"
