c02fd87a985212d0ff52056ff5e153ba71377da0
Damon Davison <damon@curatur.com>
2015-09-16 10:22:42 -0400

    Add support for various syntaxes incl CSS, HTML, ERB

--- colorize.sh
+++ colorize.sh
@@ -16,8 +16,17 @@ mkdir -p $target_dir
 function colorize() {
   file=$1
   case "$file" in
-    *.rb) lexer="ruby"; extension=".rb" ;;
-    *.js) lexer="js"; extension=".js" ;;
+    *.rb)       lexer="ruby";     extension=".rb" ;;
+    *.js)       lexer="js";       extension=".js" ;;
+    *.coffee)   lexer="coffee";   extension=".coffee" ;;
+    *.css)      lexer="css";      extension=".css" ;;
+    *.html)     lexer="html";     extension=".html" ;;
+    *.scss)     lexer="scss";     extension=".scss" ;;
+    *.yaml)     lexer="yaml";     extension=".yaml" ;;
+    *.yml)      lexer="yaml";     extension=".yml" ;;
+    *.json)     lexer="json";     extension=".json" ;;
+    *.css.erb)  lexer="css+erb";  extension=".css.erb" ;;
+    *.html.erb) lexer="html+erb"; extension=".html.erb" ;;
   esac
   filename=$(basename $file $extension)${extension}.rtf
   outfile="${target_dir}/${filename}"
